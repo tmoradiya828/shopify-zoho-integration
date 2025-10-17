5e3b7462c4751a406180fe2e57aeb10c90b8903e
tmoradiya828 <65403417+tmoradiya828@users.noreply.github.com>
2025-10-17 12:41:46 +0530

    cors

--- server.js
+++ server.js
@@ -25,7 +25,27 @@ const ZOHO = {
 // This object will store the state of tracked carts.
 const trackedCarts = {};
 
+// Set middleware of CORS 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://fitmantra.co.in"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
 
+  next();
+});
 // --- API Endpoints ---
 
 /**
