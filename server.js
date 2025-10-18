321013d430762e57c3680bca72649886bf7e9021
tmoradiya828 <65403417+tmoradiya828@users.noreply.github.com>
2025-10-18 11:56:18 +0530

    access token dasdasdas

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.  .PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // --- Configuration ---
 const ZOHO = {
